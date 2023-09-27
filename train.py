97e668ab0aaf21b455bf5c30335d0cd5b4196427
meerajagota <meerajagota16@gmail.com>
2023-09-26 19:00:09 -0700

    m

--- train.py
+++ train.py
@@ -329,11 +329,12 @@ while True:
     if iter_num > max_iters:
         break
 
-if ddp:
-    destroy_process_group()
-
 if device.type == 'cuda':
     print(torch.cuda.get_device_name(0))
     print('Memory Usage:')
     print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
     print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+    
+if ddp:
+    destroy_process_group()
+
